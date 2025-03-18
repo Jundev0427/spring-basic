6ea6eba3786124ae1c9eca70e3db18ecf2d3c12f
Jundev0427 <jundev0427@gmail.com>
2025-03-19 01:00:28 +0900

    Revert "회원, 주문, 할인 도메인 객체 지향 적용 리팩토링"
    This reverts commit 8b79eb576c3838bf1a6b72869f9e6c32a7d145b1.

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,23 +1,14 @@
 package hello.core.member;
 
-import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService;
-
-    @BeforeEach
-    public void beforeEach(){
-        AppConfig appConfig = new AppConfig();
-        this.memberService = appConfig.memberService();
-    }
-
     @Test
     void join(){
         //given
+        MemberServiceImpl memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
 
         //when
